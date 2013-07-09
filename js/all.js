0256b57cf8566fb7522960ac2f86793c8883387f
Matt <mdbrad@gmail.com>
2013-07-09 16:08:49 +0100

    First pass as the mobile menu.

--- js/all.js
+++ js/all.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
 
+	/**
+	* Main Nav 
+	*/
+	$(".toggle-main-nav").bind("click", function(event) {
+		$(this).toggleClass("toggle-menu--open");
+		$(".main-nav").toggle();
+		return false;
+	});
+
+	/**
+	* Search Bar
+	*/
+	$(".toggle-search").bind("click", function(event) {
+		$(this).toggleClass("toggle-search--active");
+		$(".search-form").toggle();
+		$(".main-nav").toggle();
+		return false;
+	});
+
+	$(".search__keywords").bind("keyup", function() {
+		var search_prompt = $(".search-prompt");
+		if($(this).val().length > 0) {
+			search_prompt.show();
+		} else {
+			search_prompt.hide();
+		}
+	});
+
+	/**
+	* Library Items
+	*/
 	$(".library__item").bind("mouseover", function(event) {
 		event.stopPropagation();
 		$(this).addClass("library__item--active");
@@ -8,19 +39,6 @@ $(document).ready(function() {
 	$(".library__item").bind("mouseout", function(event) {
 		event.stopPropagation();
 		$(this).removeClass("library__item--active");
-	});
-
-	$(".toggle-main-nav").bind("click", function(event) {
-		$(this).toggleClass("toggle-menu--open");
-		$(".main-nav").toggle();
-		return false;
-	});
-
-	$(".toggle-search").bind("click", function(event) {
-		$(this).toggleClass("toggle-search--active");
-		$(".search-form").toggle();
-		$(".main-nav").toggle();
-		return false;
 	});
 
 });
