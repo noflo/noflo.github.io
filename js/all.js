ade7ea029f9769cd563b2b7250eca27d9147bb97
Matt <mdbrad@gmail.com>
2013-07-09 17:12:22 +0100

    Added helper function for dealing with window resizes, so the menu isn't hidden/showing when it should be doing the opposite.

--- js/all.js
+++ js/all.js
@@ -1,44 +1,108 @@
+var UI = {
+
+	"site": {
+		"width": document.documentElement.clientWidth, 
+		"height": $(".container").height(), 
+		"breakpoints": [
+			{ "name": "medium", "min": 767 }, 
+			{ "name": "small", "min": 0 }
+		], 
+		"mode": {}
+	}, 
+
+	init: function() {
+
+		/**
+		* Set up various event listeners
+		*/
+
+		/**
+		* Site Mode
+		*/
+		$(window).bind("resize", function() {
+			UI.site.width = document.documentElement.clientWidth;
+			UI.getSiteMode();
+
+			if(UI.site.mode == "small") {
+				UI.resetMobileNav();
+			} else {
+				UI.resetDesktopNav();
+			}
+
+		});
+		UI.getSiteMode();
+
+		/**
+		* Main Nav 
+		*/
+		$(".toggle-main-nav").bind("click", function(event) {
+			$(this).toggleClass("toggle-menu--open");
+			$(".main-nav").toggle();
+			return false;
+		});
+
+		/**
+		* Search Bar
+		*/
+		$(".toggle-search").bind("click", function(event) {
+			$(this).toggleClass("toggle-search--active");
+			$(".search-form").toggle();
+			$(".main-nav").toggle();
+			return false;
+		});
+
+		$(".search__keywords").bind("keyup", function() {
+			var search_prompt = $(".search-prompt");
+			if($(this).val().length > 0) {
+				search_prompt.show();
+			} else {
+				search_prompt.hide();
+			}
+		});
+
+		/**
+		* Library Items
+		*/
+		$(".library__item").bind("mouseover", function(event) {
+			event.stopPropagation();
+			$(this).addClass("library__item--active");
+		});
+
+		$(".library__item").bind("mouseout", function(event) {
+			event.stopPropagation();
+			$(this).removeClass("library__item--active");
+		});
+
+	}, 
+
+	getSiteMode: function() {
+		for(var i=0, len=UI.site.breakpoints.length; i<len; i++) {
+            var breakpoint = UI.site.breakpoints[i];
+            if(document.documentElement.clientWidth > breakpoint.min) {
+                UI.site.mode = breakpoint.name;
+                break;
+            }
+        }
+	},
+
+	resetMobileNav: function() {
+		$(".toggle-main-nav").removeClass("toggle-menu--open");
+		$(".main-nav").hide();
+		UI.resetSearchBar();
+	}, 
+
+	resetDesktopNav: function() {
+		$(".main-nav").show();
+		UI.resetSearchBar();
+	}, 
+
+	resetSearchBar: function() {
+		$(".toggle-search").removeClass("toggle-search--active");
+		$(".search-form").hide();
+	}
+
+}
+
 $(document).ready(function() {
-
-	/**
-	* Main Nav 
-	*/
-	$(".toggle-main-nav").bind("click", function(event) {
-		$(this).toggleClass("toggle-menu--open");
-		$(".main-nav").toggle();
-		return false;
-	});
-
-	/**
-	* Search Bar
-	*/
-	$(".toggle-search").bind("click", function(event) {
-		$(this).toggleClass("toggle-search--active");
-		$(".search-form").toggle();
-		$(".main-nav").toggle();
-		return false;
-	});
-
-	$(".search__keywords").bind("keyup", function() {
-		var search_prompt = $(".search-prompt");
-		if($(this).val().length > 0) {
-			search_prompt.show();
-		} else {
-			search_prompt.hide();
-		}
-	});
-
-	/**
-	* Library Items
-	*/
-	$(".library__item").bind("mouseover", function(event) {
-		event.stopPropagation();
-		$(this).addClass("library__item--active");
-	});
-
-	$(".library__item").bind("mouseout", function(event) {
-		event.stopPropagation();
-		$(this).removeClass("library__item--active");
-	});
-
+	UI.init();
 });
